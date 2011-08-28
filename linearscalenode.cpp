acfa6debdaf0623117e7c5bac0c73bd3a11f4c7d
Jérémie Delaitre <jeremie.delaitre@gmail.com>
2011-08-28 22:15:14 +0200

    Start handling of the scale painting thickness

--- linearscalenode.cpp
+++ linearscalenode.cpp
@@ -5,7 +5,7 @@
 LinearScaleNode::LinearScaleNode()
     : m_geometry(QSGGeometry::defaultAttributes_Point2D(), 0)
 {
-    m_geometry.setDrawingMode(GL_LINES);
+    m_geometry.setDrawingMode(GL_QUADS);
     m_material.setColor(Qt::black);
     setMaterial(&m_material);
     setGeometry(&m_geometry);
@@ -43,49 +43,132 @@ void LinearScaleNode::updateFromLinearScale(LinearScale* scale)
     //for (int i = 0; i < 30; ++i)
     //    ticks << (i * scaleMap->pixelLength() / 30);
     int tickCount = ticks.count();
+    double baselineThickness = 3;
+    double tickThickness = 1;
 
-    m_geometry.allocate(tickCount * 2 + 2); // 2 vertices per tick + 2 vertices for the baseline
+    // Draw each tick with three rectangles:
+    //  - one for the baseline part to go the tick
+    //  - one for the tick itself
+    //  - one for the part owned by both the baseline and the tick
+
+    // vertical:              horizontal:
+    // .----.--.              .------.--.
+    // |tick|  |              |  bl  |  |
+    // .----.--.              .------.--.
+    //      |  |                     |ti|
+    //      |bl|                     |ck|
+    //      |  |                     .--.
+    //      .--.
+
+    // Then, draw a rectangle for the end of the baseline
+    m_geometry.allocate(tickCount * 4 * 3 + 4);
     QSGGeometry::Point2D* vertex = m_geometry.vertexDataAsPoint2D();
 
-    // Ticks
-    for (int i = 0; i < tickCount; ++i)
-    {
-        double pixelPos = scaleMap->mapToPixel(ticks[i]);
-
-        if (orientation == Qt::Horizontal)
-        {
-            vertex[i * 2].x = pixelPos;
-            vertex[i * 2].y = height;
-            vertex[i * 2 + 1].x = pixelPos;
-            vertex[i * 2 + 1].y = 0.;
-        }
-        else
-        {
-            pixelPos = height - pixelPos; // y = 0 is at the top, so we subtract from the height
-
-            vertex[i * 2].x = width;
-            vertex[i * 2].y = pixelPos;
-            vertex[i * 2 + 1].x = 0;
-            vertex[i * 2 + 1].y = pixelPos;
-        }
-    }
-
-    // Baseline
-    QSGGeometry::Point2D& baselineFrom = vertex[m_geometry.vertexCount() - 2];
-    QSGGeometry::Point2D& baselineTo = vertex[m_geometry.vertexCount() - 1];
     if (orientation == Qt::Horizontal)
     {
-        baselineFrom.x = 0;
-        baselineFrom.y = 0;
-        baselineTo.x = width;
-        baselineTo.y = 0;
+        double lastPos = 0.;
+
+        for (int i = 0; i < tickCount; ++i)
+        {
+            double tickPos = scaleMap->mapToPixel(ticks[i]);
+            double tickPosFrom = tickPos - tickThickness / 2.;
+            double tickPosTo = tickPos + tickThickness / 2.;
+
+            // Baseline to the tick
+            vertex[i * 12 + 0].x = lastPos;
+            vertex[i * 12 + 0].y = 0.;
+            vertex[i * 12 + 1].x = tickPosFrom;
+            vertex[i * 12 + 1].y = 0.;
+            vertex[i * 12 + 2].x = tickPosFrom;
+            vertex[i * 12 + 2].y = baselineThickness;
+            vertex[i * 12 + 3].x = lastPos;
+            vertex[i * 12 + 3].y = baselineThickness;
+
+            // Part owned by both
+            vertex[i * 12 + 4].x = tickPosFrom;
+            vertex[i * 12 + 4].y = 0.;
+            vertex[i * 12 + 5].x = tickPosTo;
+            vertex[i * 12 + 5].y = 0.;
+            vertex[i * 12 + 6].x = tickPosTo;
+            vertex[i * 12 + 6].y = baselineThickness;
+            vertex[i * 12 + 7].x = tickPosFrom;
+            vertex[i * 12 + 7].y = baselineThickness;
+
+            // Tick
+            vertex[i * 12 + 8].x = tickPosFrom;
+            vertex[i * 12 + 8].y = baselineThickness;
+            vertex[i * 12 + 9].x = tickPosTo;
+            vertex[i * 12 + 9].y = baselineThickness;
+            vertex[i * 12 + 10].x = tickPosTo;
+            vertex[i * 12 + 10].y = height;
+            vertex[i * 12 + 11].x = tickPosFrom;
+            vertex[i * 12 + 11].y = height;
+
+            lastPos = tickPosTo;
+        }
+
+        // End of the baseline
+        vertex[tickCount * 12 + 0].x = lastPos;
+        vertex[tickCount * 12 + 0].y = 0.;
+        vertex[tickCount * 12 + 1].x = width;
+        vertex[tickCount * 12 + 1].y = 0.;
+        vertex[tickCount * 12 + 2].x = width;
+        vertex[tickCount * 12 + 2].y = baselineThickness;
+        vertex[tickCount * 12 + 3].x = lastPos;
+        vertex[tickCount * 12 + 3].y = baselineThickness;
     }
     else
     {
-        baselineFrom.x = width;
-        baselineFrom.y = height;
-        baselineTo.x = width;
-        baselineTo.y = 0;
+        double lastPos = height;
+
+        for (int i = 0; i < tickCount; ++i)
+        {
+            double tickPos = height - scaleMap->mapToPixel(ticks[i]);
+            double tickPosFrom = tickPos + tickThickness / 2.;
+            double tickPosTo = tickPos - tickThickness / 2.;
+
+            // Baseline to the tick
+            vertex[i * 12 + 0].x = width;
+            vertex[i * 12 + 0].y = lastPos;
+            vertex[i * 12 + 1].x = width;
+            vertex[i * 12 + 1].y = tickPosFrom;
+            vertex[i * 12 + 2].x = width - baselineThickness;
+            vertex[i * 12 + 2].y = tickPosFrom;
+            vertex[i * 12 + 3].x = width - baselineThickness;
+            vertex[i * 12 + 3].y = lastPos;
+
+            // Part owned by both
+            vertex[i * 12 + 4].x = width;
+            vertex[i * 12 + 4].y = tickPosFrom;
+            vertex[i * 12 + 5].x = width;
+            vertex[i * 12 + 5].y = tickPosTo;
+            vertex[i * 12 + 6].x = width - baselineThickness;
+            vertex[i * 12 + 6].y = tickPosTo;
+            vertex[i * 12 + 7].x = width - baselineThickness;
+            vertex[i * 12 + 7].y = tickPosFrom;
+
+            // Tick
+            vertex[i * 12 + 8].x = width - baselineThickness;
+            vertex[i * 12 + 8].y = tickPosFrom;
+            vertex[i * 12 + 9].x = width - baselineThickness;
+            vertex[i * 12 + 9].y = tickPosTo;
+            vertex[i * 12 + 10].x = 0.;
+            vertex[i * 12 + 10].y = tickPosTo;
+            vertex[i * 12 + 11].x = 0.;
+            vertex[i * 12 + 11].y = tickPosFrom;
+
+            lastPos = tickPosTo;
+        }
+
+        // End of the baseline
+        vertex[tickCount * 12 + 0].x = width;
+        vertex[tickCount * 12 + 0].y = lastPos;
+        vertex[tickCount * 12 + 1].x = width;
+        vertex[tickCount * 12 + 1].y = 0.;
+        vertex[tickCount * 12 + 2].x = width - baselineThickness;
+        vertex[tickCount * 12 + 2].y = 0.;
+        vertex[tickCount * 12 + 3].x = width - baselineThickness;
+        vertex[tickCount * 12 + 3].y = lastPos;
     }
 
     markDirty(QSGNode::DirtyGeometry);
